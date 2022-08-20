8c05b4141264eba02b12d69f458b6beafca53a4e
bryant fowler <fowler1221bryant@gmail.com>
2022-08-20 19:54:47 -0400

    add thoughts api

--- models/index.js
+++ models/index.js
@@ -1,1 +1,10 @@
 const User = require("./User");
+
+const userRoutes = require("./user-routes");
+const thoughtRoutes = require("./thoughts-routes");
+const router = require("../routes");
+
+router.use("/api/users", userRoutes);
+router.use("/api/thoughts", thoughtRoutes);
+
+module.exports = router;
